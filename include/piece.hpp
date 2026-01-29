727c1a658e7623a640e775910d74dee3e4aec349
ashi mashi <mahdi.mm2006@gmail.com>
2026-01-29 18:25:28 +0330

    piece done

--- include/piece.hpp
+++ include/piece.hpp
@@ -1,6 +1,25 @@
 #ifndef PEACE_HPP
 #define PIECE_HPP
 
-class Piece{};
+#include <iostream>
+
+class Piece{
+    private:
+        std::string color;
+        std::string Location;
+        std::string Name;
+
+    public:
+        Piece(const std::string& Location, const std::string& color);
+        ~Piece();
+        std::string get_Location() const;
+        std::string getColor() const;
+        std::string getName() const;
+        void set_Location(const std::string& loctaion);
+        void setColor(const std::string& color);
+        virtual void draw() = 0;
+        virtual void move(std::string& origin, std::string& destination, Piece& piece) = 0;
+    
+};
 
 #endif //PEACE_HPP
--- src/piece.cpp
+++ src/piece.cpp
@@ -1,1 +1,21 @@
 #include "piece.hpp"
+
+Piece::Piece(const std::string& Location, const std::string& color) : Location(Location), color(color){}
+Piece::~Piece(){}
+
+void Piece::set_Location(const std::string& Location){
+    this->Location = Location;
+}
+void Piece::setColor(const std::string& color){
+    this->color = color;
+}
+std::string Piece::getColor() const{
+    return color;
+}
+std::string Piece::get_Location() const{
+    return Location;
+}
+std::string Piece::getName()const{
+    return Name;
+}
+
